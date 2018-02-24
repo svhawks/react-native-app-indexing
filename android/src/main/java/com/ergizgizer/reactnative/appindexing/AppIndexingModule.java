e366fecaf7da91f36e24fa927688e9a7f08ff61a
Ergiz Gizer <ergizgizer@gmail.com>
2018-02-24 16:12:10 +0300

    Refactor the module

--- android/src/main/java/com/ergizgizer/reactnative/appindexing/AppIndexingModule.java
+++ android/src/main/java/com/ergizgizer/reactnative/appindexing/AppIndexingModule.java
@@ -3,11 +3,8 @@ package com.ergizgizer.reactnative.appindexing;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
 
 import org.json.JSONArray;
-import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -31,12 +28,7 @@ public class AppIndexingModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void syncStickers(JSONObject firebaseOptions, JSONArray stickerPack){
-        FirebaseOptions options = new FirebaseOptions.Builder()
-                .setApiKey(FIRBEASE_API_KEY)
-                .build();
-        FirebaseApp.initializeApp(getReactApplicationContext(), options);
-
+    public void syncStickers(JSONArray stickerPack){
         AppIndexingUpdateService.enqueueWork(getReactApplicationContext(), stickerPack);
     }
 }
