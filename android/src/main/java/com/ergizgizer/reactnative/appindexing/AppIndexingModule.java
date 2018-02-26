7cd946aef037e432857ebf229cb7a76b8b8aa255
Ergiz Gizer <ergizgizer@gmail.com>
2018-02-26 22:10:34 +0300

    Add firebase initialization method

--- android/src/main/java/com/ergizgizer/reactnative/appindexing/AppIndexingModule.java
+++ android/src/main/java/com/ergizgizer/reactnative/appindexing/AppIndexingModule.java
@@ -1,17 +1,44 @@
 package com.ergizgizer.reactnative.appindexing;
 
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.WritableMap;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 
 public class AppIndexingModule extends ReactContextBaseJavaModule {
+
+    private static final String RESULT_KEY = "result";
+    private boolean appInitialized;
 
     public AppIndexingModule(ReactApplicationContext reactContext) { super(reactContext); }
 
     @Override
     public String getName() {
         return "AppIndexingModule";
+    }
+
+    @ReactMethod
+    public void initializeApp(String appName, ReadableMap options, Callback callback) {
+        if (appInitialized) return;
+
+        FirebaseOptions.Builder builder = new FirebaseOptions.Builder();
+
+        builder.setApiKey(options.getString("apiKey"));
+        builder.setApplicationId(options.getString("appId"));
+        builder.setProjectId(options.getString("projectId"));
+
+        FirebaseApp.initializeApp(getReactApplicationContext(), builder.build(), appName);
+
+        WritableMap response = Arguments.createMap();
+        response.putString(RESULT_KEY, "success");
+        callback.invoke(null, response);
+        appInitialized = true;
     }
 
     @ReactMethod
